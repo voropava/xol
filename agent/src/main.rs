6c32d99cdccab720d8907584fffc40a6c6c89750
Pavel Voronkin <pvoronkin@gridgain.com>
2019-03-13 06:20:28 +0300

    Added comment

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,5 +1,6 @@
 use std::cmp;
 
+// https://neerc.ifmo.ru/wiki/index.php?title=%D0%97%D0%B0%D0%B4%D0%B0%D1%87%D0%B0_%D0%BE_%D1%80%D0%B5%D0%B4%D0%B0%D0%BA%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D0%BE%D0%BC_%D1%80%D0%B0%D1%81%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D0%B8,_%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%92%D0%B0%D0%B3%D0%BD%D0%B5%D1%80%D0%B0-%D0%A4%D0%B8%D1%88%D0%B5%D1%80%D0%B0
 fn main() {
     let s1 = String::from("Hello");
     let s2 = String::from("wor");
